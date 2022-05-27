3647d129c0f6632ce9f06193a917c8ca354cb091
pletniri <pletniri@gmail.com>
2022-05-27 12:58:00 -0400

    The first upload

---
+++ test.py
@@ -0,0 +1,23 @@
+from app import app 
+
+def test():
+    
+'''
+This function tests that the flask application has a correct responce code when the application goes live
+
+'''
+
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+
+def test2():
+    response = app.test_client().get('/edit')
+    assert response.status_code == 200
+
+def test3():
+    pass
+"""A dummy docstring"""
+response = app.test_client().get(*/edit*)
+assert b"To-Do Application" in response.data
+assert b"Todo Title" in response.data
+assert b"Add" in response.data
