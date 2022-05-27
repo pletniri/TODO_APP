07ae45f48e662c2c300dd8b0a320cbbe850bc67c
pletniri <pletniri@gmail.com>
2022-05-27 14:07:39 -0400

    Fixed_error2

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
 from app import app 
 
 def test():
-    
-'''
-This function tests that the flask application has a correct responce code when the application goes live
-
-'''
-
     response = app.test_client().get('/')
     assert response.status_code == 200
 
